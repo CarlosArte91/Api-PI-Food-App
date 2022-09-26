2c91582206ba93ded03ec5448efec4dc0e3f7ce3
Carlos Cruz <carturo99@gmail.com>
2022-09-26 13:36:54 -0500

    actualization

--- Api/src/app.js
+++ Api/src/app.js
@@ -24,6 +24,8 @@ server.use((req, res, next) => {
 
 server.use('/api', routes);
 
+server.get("/api", (req, res) => res.send("Welcome to api food"));
+
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   const status = err.status || 500;
